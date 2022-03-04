52a25b3bd28d844c170c802df73ff7b4bea1c458
losrobbos <50549469+losrobbos@users.noreply.github.com>
2022-03-04 16:30:19 +0100

    Update 17-var-conventions.js

--- 1-js-basics/17-var-conventions.js
+++ 1-js-basics/17-var-conventions.js
@@ -3,5 +3,5 @@
 const summe = 0
 const message = "hello"
 
-const products = [] // products
-const product = {} // product
+const products = [] // array? => variable in MEHRZAHL benennen => products 
+const product = {} // object? => variable in EINZAHL benennen => product
